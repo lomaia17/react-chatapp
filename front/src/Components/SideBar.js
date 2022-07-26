c8ec1f1ff92e0a1a508ab75e61a249bf02d99501
Giorgi Lomaia <54961682+Lomaia17@users.noreply.github.com>
2022-07-26 15:47:30 +0400

    some minor bug fixing

--- front/src/Components/SideBar.js
+++ front/src/Components/SideBar.js
@@ -3,7 +3,7 @@ import Users from "./Users";
 const SideBar = ({ users, userIcon }) => {
   return (
     <div className={`w-screen lg:block lg:w-1/4 h-full bg-slate-900  py-5`}>
-      <h1 className="text-white text-2xl">Active:</h1>
+      <h1 className="text-white text-center text-2xl">Active:</h1>
 
       <div className="overflow-y-auto h-3/4 w-full scrollbar flex flex-col items-center">
         {users &&
